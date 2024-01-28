ecc5fb7fc133c1c89883f39d266872070557d6d7
ycysdf <ycy22333@163.com>
2024-01-28 22:29:37 +0800

    refactor: Refactor reactive module to support other attribute types

--- crates/rxy_bevy/src/reactive/mod.rs
+++ crates/rxy_bevy/src/reactive/mod.rs
@@ -5,6 +5,49 @@ use std::marker::PhantomData;
 
 pub struct IntoAttrMemberWrapper<T, M>(T, PhantomData<M>);
 
+impl<EA, F, IA> IntoViewAttrMember<EA> for IntoAttrMemberWrapper<rxy_core::Reactive<F, IA>, EA>
+where
+    EA: ElementUnitAttr,
+    F: Fn() -> IA + Send + 'static,
+    IA: IntoViewAttrMember<EA> + Send + 'static,
+{
+    type Attr = Self;
+
+    fn into_attr(self) -> Self::Attr {
+        self
+    }
+
+    type OtherAttr<OEA: ElementUnitAttr<Value = <EA>::Value>> = IntoAttrMemberWrapper<
+        rxy_core::Reactive<Box<dyn Fn() -> IA::OtherAttr<OEA> + Send>, IA::OtherAttr<OEA>>,
+        OEA,
+    >;
+
+    fn into_other_attr<OEA: ElementUnitAttr<Value = <EA>::Value>>(self) -> Self::OtherAttr<OEA> {
+        IntoAttrMemberWrapper(
+            rx(Box::new(move|| self.0 .0().into_other_attr())),
+            Default::default(),
+        )
+    }
+}
+
+// impl<EA, T> IntoViewAttrMember<EA> for IntoAttrMemberWrapper<T, EA>
+// where
+//     EA: ElementUnitAttr,
+//     T: IntoViewAttrMember<EA> + ViewMember<BevyRenderer>,
+// {
+//     type Attr = T;
+
+//     fn into_attr(self) -> Self::Attr {
+//         self.0
+//     }
+
+//     type OtherAttr<OEA: ElementUnitAttr<Value = <EA>::Value>> = T::OtherAttr<OEA>;
+
+//     fn into_other_attr<OEA: ElementUnitAttr<Value = <EA>::Value>>(self) -> Self::OtherAttr<OEA> {
+//         self.0.into_other_attr::<OEA>()
+//     }
+// }
+
 impl<IA, F, EA> IntoViewAttrMember<EA> for rxy_core::Reactive<F, IA>
 where
     EA: ElementUnitAttr,
@@ -15,6 +58,18 @@ where
 
     fn into_attr(self) -> Self::Attr {
         IntoAttrMemberWrapper(self, Default::default())
+    }
+
+    type OtherAttr<OEA: ElementUnitAttr<Value = <EA>::Value>> = IntoAttrMemberWrapper<
+        rxy_core::Reactive<Box<dyn Fn() -> IA::OtherAttr<OEA> + Send>, IA::OtherAttr<OEA>>,
+        OEA,
+    >;
+
+    fn into_other_attr<OEA: ElementUnitAttr<Value = <EA>::Value>>(self) -> Self::OtherAttr<OEA> {
+        IntoAttrMemberWrapper(
+            rx(Box::new(move || self.0().into_other_attr())),
+            Default::default(),
+        )
     }
 }
 
@@ -58,6 +113,31 @@ macro_rules! impl_into_view_attr_member_for_signal {
 
                 fn into_attr(self) -> Self::Attr {
                     IntoAttrMemberWrapper(self, Default::default())
+                }
+
+                type OtherAttr<OEA: ElementUnitAttr<Value = <EA>::Value>> = IntoAttrMemberWrapper<rxy_core::Reactive<Box<dyn Fn() -> IA::OtherAttr<OEA> + Send>,IA::OtherAttr<OEA>>, OEA>;
+
+                fn into_other_attr<OEA: ElementUnitAttr<Value = <EA>::Value>>(self) -> Self::OtherAttr<OEA> {
+                    use xy_reactive::prelude::SignalGet;
+                    IntoAttrMemberWrapper(rx(Box::new(move || self.get().into_other_attr::<OEA>())), Default::default())
+                }
+            }
+
+            impl<EA, IA> IntoViewAttrMember<EA> for IntoAttrMemberWrapper<$ty::<IA>, EA>
+            where
+                EA: ElementUnitAttr,
+                IA: IntoViewAttrMember<EA> + Clone + Send + Sync + 'static,
+            {
+                type Attr = IntoAttrMemberWrapper<$ty::<IA>, EA>;
+
+                fn into_attr(self) -> Self::Attr {
+                    self
+                }
+
+                type OtherAttr<OEA: ElementUnitAttr<Value = <EA>::Value>> = IntoAttrMemberWrapper<rxy_core::Reactive<Box<dyn Fn() -> IA::OtherAttr<OEA> + Send>,IA::OtherAttr<OEA>>, OEA>;
+
+                fn into_other_attr<OEA: ElementUnitAttr<Value = <EA>::Value>>(self) -> Self::OtherAttr<OEA> {
+                    self.0.into_other_attr::<OEA>()
                 }
             }
 
--- crates/rxy_bevy/src/view_member/into_attr.rs
+++ crates/rxy_bevy/src/view_member/into_attr.rs
@@ -1,62 +1,209 @@
-use futures_lite::StreamExt;
-use rxy_bevy_element::ElementUnitAttr;
-use rxy_core::{Either, ViewMember};
-use crate::{BevyRenderer, BevyWrapper, ViewAttr};
+use core::marker::PhantomData;
 
-pub trait IntoViewAttrMember<EA> {
-    // type Attr: ViewAttrMember<EA = EA>;
-    type Attr: ViewMember<BevyRenderer>;
+use crate::{BevyRenderer, BevyWrapper, ViewAttr};
+use bevy_reflect::{FromReflect, TypePath};
+use futures_lite::{prelude::Future, StreamExt};
+use rxy_bevy_element::{AttrIndex, AttrValue, ElementAttr, ElementUnitAttr, HasIndex};
+use rxy_core::{Either, ViewMember, XFuture};
+
+pub struct ElementAttrAgent<EAV>(PhantomData<EAV>);
+
+impl<EAV> HasIndex for ElementAttrAgent<EAV> {
+    const INDEX: AttrIndex = u8::MAX;
+}
+
+impl<EAV> ElementAttr for ElementAttrAgent<EAV>
+where
+    EAV: AttrValue + Clone + Sized + FromReflect + TypePath,
+{
+    type Value = EAV;
+
+    const NAME: &'static str = "agent";
+
+    fn set_value(
+        _context: &mut rxy_bevy_element::SetAttrValueContext,
+        _value: impl Into<Self::Value>,
+    ) {
+        unreachable!("agent attr should not be set")
+    }
+}
+
+pub trait IntoViewAttrMember<EA>
+where
+    EA: ElementUnitAttr,
+{
+    type Attr: ViewMember<BevyRenderer> + IntoViewAttrMember<EA>;
+    type OtherAttr<OEA: ElementUnitAttr<Value = EA::Value>>: ViewMember<BevyRenderer>
+        + IntoViewAttrMember<OEA>;
+
     fn into_attr(self) -> Self::Attr;
+
+    fn into_other_attr<OEA: ElementUnitAttr<Value = EA::Value>>(self) -> Self::OtherAttr<OEA>;
+}
+
+impl<EA> IntoViewAttrMember<EA> for ViewAttr<EA>
+where
+    EA: ElementUnitAttr,
+{
+    type Attr = Self;
+
+    fn into_attr(self) -> Self::Attr {
+        self
+    }
+
+    type OtherAttr<OEA: ElementUnitAttr<Value = <EA>::Value>> = ViewAttr<OEA>;
+
+    fn into_other_attr<OEA: ElementUnitAttr<Value = <EA>::Value>>(self) -> Self::OtherAttr<OEA> {
+        ViewAttr(self.0)
+    }
 }
 
 impl<EA, T> IntoViewAttrMember<EA> for T
-    where
-        EA: ElementUnitAttr,
-        T: Into<BevyWrapper<EA::Value>>,
+where
+    EA: ElementUnitAttr,
+    T: Into<BevyWrapper<EA::Value>>,
 {
     type Attr = ViewAttr<EA>;
 
     fn into_attr(self) -> Self::Attr {
         ViewAttr(self.into().0)
     }
+
+    type OtherAttr<OEA: ElementUnitAttr<Value = <EA>::Value>> = ViewAttr<OEA>;
+
+    fn into_other_attr<OEA: ElementUnitAttr<Value = <EA>::Value>>(self) -> Self::OtherAttr<OEA> {
+        ViewAttr(self.into().0)
+    }
 }
 
 impl<EA, T> IntoViewAttrMember<EA> for Option<T>
-    where
-        EA: ElementUnitAttr,
-        T: Into<BevyWrapper<EA::Value>>,
+where
+    EA: ElementUnitAttr,
+    T: IntoViewAttrMember<EA>,
 {
-    type Attr = Option<ViewAttr<EA>>;
+    type Attr = Option<T::Attr>;
 
     fn into_attr(self) -> Self::Attr {
-        self.map(|n| ViewAttr(n.into().0))
+        self.map(|n| n.into_attr())
+    }
+
+    type OtherAttr<OEA: ElementUnitAttr<Value = <EA>::Value>> = Option<T::OtherAttr<OEA>>;
+
+    fn into_other_attr<OEA: ElementUnitAttr<Value = <EA>::Value>>(self) -> Self::OtherAttr<OEA> {
+        self.map(|n| n.into_other_attr::<OEA>())
     }
 }
 
 impl<EA, L, R> IntoViewAttrMember<EA> for Either<L, R>
-    where
-        EA: ElementUnitAttr,
-        L: Into<BevyWrapper<EA::Value>>,
-        R: Into<BevyWrapper<EA::Value>>,
+where
+    EA: ElementUnitAttr,
+    L: IntoViewAttrMember<EA>,
+    R: IntoViewAttrMember<EA>,
 {
-    type Attr = Either<ViewAttr<EA>, ViewAttr<EA>>;
+    type Attr = Either<L::Attr, R::Attr>;
 
     fn into_attr(self) -> Self::Attr {
         match self {
-            Either::Left(n) => Either::Left(ViewAttr(n.into().0)),
-            Either::Right(n) => Either::Right(ViewAttr(n.into().0)),
+            Either::Left(n) => Either::Left(n.into_attr()),
+            Either::Right(n) => Either::Right(n.into_attr()),
+        }
+    }
+
+    type OtherAttr<OEA: ElementUnitAttr<Value = <EA>::Value>> =
+        Either<L::OtherAttr<OEA>, R::OtherAttr<OEA>>;
+
+    fn into_other_attr<OEA: ElementUnitAttr<Value = <EA>::Value>>(self) -> Self::OtherAttr<OEA> {
+        match self {
+            Either::Left(n) => Either::Left(n.into_other_attr::<OEA>()),
+            Either::Right(n) => Either::Right(n.into_other_attr::<OEA>()),
         }
     }
 }
 
 impl<EA, T> IntoViewAttrMember<EA> for futures_lite::stream::Boxed<T>
-    where
-        EA: ElementUnitAttr,
-        T: IntoViewAttrMember<EA> + 'static,
+where
+    EA: ElementUnitAttr,
+    T: IntoViewAttrMember<EA> + 'static,
 {
     type Attr = futures_lite::stream::Boxed<T::Attr>;
 
     fn into_attr(self) -> Self::Attr {
         self.map(|n| n.into_attr()).boxed()
+    }
+
+    type OtherAttr<OEA: ElementUnitAttr<Value = <EA>::Value>> =
+        futures_lite::stream::Boxed<T::OtherAttr<OEA>>;
+
+    fn into_other_attr<OEA: ElementUnitAttr<Value = <EA>::Value>>(self) -> Self::OtherAttr<OEA> {
+        self.map(|n| n.into_other_attr()).boxed()
+    }
+}
+
+impl<EA, T> IntoViewAttrMember<EA> for XFuture<T>
+where
+    EA: ElementUnitAttr,
+    T: Future + Send + 'static,
+    T::Output: IntoViewAttrMember<EA> + Send + 'static,
+{
+    type Attr = XFuture<futures_lite::future::Boxed<<T::Output as IntoViewAttrMember<EA>>::Attr>>;
+
+    fn into_attr(self) -> Self::Attr {
+        use futures_lite::FutureExt;
+        XFuture(
+            (async {
+                let n = self.0.await;
+                n.into_attr()
+            })
+            .boxed(),
+        )
+    }
+
+    type OtherAttr<OEA: ElementUnitAttr<Value = <EA>::Value>> =
+        XFuture<futures_lite::future::Boxed<<T::Output as IntoViewAttrMember<EA>>::OtherAttr<OEA>>>;
+
+    fn into_other_attr<OEA: ElementUnitAttr<Value = <EA>::Value>>(self) -> Self::OtherAttr<OEA> {
+        use futures_lite::FutureExt;
+
+        XFuture(
+            (async {
+                let n = self.0.await;
+                n.into_other_attr::<OEA>()
+            })
+            .boxed(),
+        )
+    }
+}
+
+impl<EA, T> IntoViewAttrMember<EA> for futures_lite::future::Boxed<T>
+where
+    EA: ElementUnitAttr,
+    T: IntoViewAttrMember<EA> + Send + 'static,
+{
+    type Attr = XFuture<futures_lite::future::Boxed<T::Attr>>;
+
+    fn into_attr(self) -> Self::Attr {
+        use futures_lite::FutureExt;
+        XFuture(
+            (async {
+                let n = self.await;
+                n.into_attr()
+            })
+            .boxed(),
+        )
+    }
+
+    type OtherAttr<OEA: ElementUnitAttr<Value = <EA>::Value>> =
+        XFuture<futures_lite::future::Boxed<T::OtherAttr<OEA>>>;
+
+    fn into_other_attr<OEA: ElementUnitAttr<Value = <EA>::Value>>(self) -> Self::OtherAttr<OEA> {
+        use futures_lite::FutureExt;
+
+        XFuture(
+            (async {
+                let n = self.await;
+                n.into_other_attr::<OEA>()
+            })
+            .boxed(),
+        )
     }
 }
